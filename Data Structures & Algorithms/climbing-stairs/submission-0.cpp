17e544a3530e843d0a376bdc9c6bfb5bd7d371d4
TheKB123 <ncris2709@gmail.com>
2026-09-15 22:34:03 +0300

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> memo = vector<int>(46, 0);
+    int climbStairs(int n) {
+        if ( n < 0 )
+            return 0;
+        if ( n < 2 )
+            return 1;
+        if ( memo[n] )
+            return memo[n];
+        return memo[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+};
