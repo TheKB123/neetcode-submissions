cf65673dca9b0dc5a27d1620d15fa0a0ae131df8
TheKB123 <ncris2709@gmail.com>
2026-09-22 11:09:24 +0300

    Add: single-element-in-a-sorted-array - submission-3

---
+++ Data Structures & Algorithms/single-element-in-a-sorted-array/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, m, r = nums.size(), n = nums.size() - 1;
+        do {
+            m = l + ( r - l ) / 2;
+            if ( m == 0 || m == n || nums[m] != nums[m-1] && nums[m] != nums[m+1] )
+                break;
+            if ( ( m - ( nums[m-1] == nums[m] ) ) % 2 )
+                r = m - 1 - (nums[m-1] == nums[m]);
+            else
+                l = m + 1 + (nums[m] == nums[m+1]);
+        } while ( l <= r );
+        return nums[m];
+    }
+};
